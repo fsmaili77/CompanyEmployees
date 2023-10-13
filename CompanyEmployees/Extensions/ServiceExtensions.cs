53be51d5a73693e96e4dde1320c23937826d627c
Fehmir <fehmir@gmail.com>
2023-10-13 16:55:20 +0200

    Initial configuration: launchSettings, ServiceExtensions (CORS, Logging)

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        // CORS
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                // Decide which origins, methods or headers to allow : builder.AllowAnyOrigin() or .AllowAnyMethod() or .AllowAnyHeader()
+                builder.WithOrigins("https://example.com")
+                .WithMethods("POST", "GET") // or PUT, DELETE
+                .WithHeaders("accept", "content-type"));
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+            });
+
+        // Logger
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+
+    }
+}
