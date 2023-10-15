e3a14287efff695aefbc4a18851c24d7b51f5440
Fehmir <fehmir@gmail.com>
2023-10-16 00:33:54 +0200

    Add CompanyEmployees.Presentation project (CompaniesController with GetCompanies method), add reference to required projects of the app

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -14,5 +14,11 @@ namespace Repository
             : base(repositoryContext)
         {
         }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+            FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
+        
     }
 }
--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities.Models;
 using Service.Contracts;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,21 @@ namespace Service
         {
             _repository = repository;
             _logger = logger;
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+            try
+            {
+                var companies =
+                _repository.Company.GetAllCompanies(trackChanges);
+                return companies;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the { nameof(GetAllCompanies)} service method { ex}");
+            throw;
+            }
         }
     }
 }
