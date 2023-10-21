1c84a298a65a739af12af4d195c59e494bfb9f22
Fehmir <fehmir@gmail.com>
2023-10-21 22:29:38 +0200

    Creating a collection of resources; Model binding in API

---
+++ Entities/Exceptions/IdParametersBadRequestException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public sealed class IdParametersBadRequestException : BadRequestException
+    {
+        public IdParametersBadRequestException()
+        : base("Parameter ids is null")
+        {
+        }
+    }
+
+    public sealed class CollectionByIdsBadRequestException : BadRequestException
+    {
+        public CollectionByIdsBadRequestException()
+        : base("Collection count mismatch comparing to ids.")
+        {
+        }
+    }
+}
