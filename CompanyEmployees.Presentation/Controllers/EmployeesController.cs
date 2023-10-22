bd7c295de56ab53b09cf898b1b18013ff7276b31
Fehmir <fehmir@gmail.com>
2023-10-22 14:46:52 +0200

    Add Delete requests both for Employee and Company

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -46,5 +46,13 @@ namespace CompanyEmployees.Presentation.Controllers
                 new {companyId, id = employeeToReturn.Id }, 
                 employeeToReturn);
         }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id)
+        {
+            _service.EmployeeService.DeleteEmployeeForCompany(companyId, id, trackChanges: false);
+
+            return NoContent();
+        }
     }
 }
