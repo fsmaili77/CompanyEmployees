a9652094a6bcbc3965af29e8dcf14ade85409722
Fehmir <fehmir@gmail.com>
2023-11-04 12:48:01 +0100

    Add Filtering

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -26,8 +26,14 @@ namespace Service
             _mapper = mapper;
         }        
 
-        public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
+        public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, 
+            EmployeeParameters employeeParameters, bool trackChanges)
         {
+            if (!employeeParameters.ValidAgeRange)
+            {
+                throw new MaxAgeRangeBadRequestException();
+            }
+
             await CheckIfCompanyExists(companyId, trackChanges);
 
             var employeesWithMetaData = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges);
